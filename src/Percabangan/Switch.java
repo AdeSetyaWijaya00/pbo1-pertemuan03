920693f662cf90aaa3fc7bce38d4a4ba4bb41dbe
AdeSetyaWijaya00 <AdeSetyaWijaya00@gmail.com>
2020-04-10 10:40:13 +0800

    Membuat Switch

---
+++ src/Percabangan/Switch.java
@@ -0,0 +1,39 @@
+package Percabangan;
+
+public class Switch {
+    public static void main(String[] args) {
+        int uang = 1;
+        switch (uang) {
+            case 1 :
+                System.out.println("Rp.100,00");
+                break;
+            case 2 :
+                System.out.println("Rp.500,00");
+                break;
+            case 3 :
+                System.out.println("Rp.1000,00");
+                break;
+            case 4 :
+                System.out.println("Rp.5000,00");
+                break;
+            case 5 :
+                System.out.println("Rp.10.000,00");
+                break;
+            case 6 :
+                System.out.println("Rp.20.000,00");
+                break;
+            case 7 :
+                System.out.println("Rp.50.000,00");
+                break;
+            case 8 :
+                System.out.println("Rp.100.000,00");
+                break;
+            case 9 :
+                System.out.println("Sultan");
+                break;
+            default:
+                break;
+
+        }
+    }
+}
