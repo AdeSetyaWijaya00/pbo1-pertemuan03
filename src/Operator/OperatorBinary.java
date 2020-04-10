0fb9d5b25bb7d0e0816fb97855f940c55b4cbfd1
AdeSetyaWijaya00 <AdeSetyaWijaya00@gmail.com>
2020-04-10 09:43:42 +0800

    Operator Binary Aritmatika

--- src/Operator/OperatorBinary.java
+++ src/Operator/OperatorBinary.java
@@ -5,4 +5,13 @@ public class OperatorBinary {
     String nama = "Ade Setya Wijaya";
     int jumlahSks = 24;
     double indeksPrestasiKumulatif = 3.80;
+
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
 }
