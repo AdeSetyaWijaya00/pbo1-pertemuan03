4b76031c290e13792568b31cbda00d042992f622
AdeSetyaWijaya00 <AdeSetyaWijaya00@gmail.com>
2020-04-10 10:27:55 +0800

    Membuat If Else

---
+++ src/Percabangan/IfElse.java
@@ -0,0 +1,15 @@
+package Percabangan;
+
+public class IfElse {
+    public static void main(String[] args) {
+        int x = 5;
+
+        if (x > 6) System.out.println("x>i");
+        else if (x <= 4)
+            System.out.println("x<=4");
+        else if (x < 10)
+            System.out.println("x<10");
+        else
+            System.out.println("bukan dari salah satu di atas");
+    }
+}
